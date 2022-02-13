cad18f58d756aab6a37bb10ff3eab201d4f51642
vishalkumar1999 <vk137rk@gmail.com>
2022-02-13 22:51:55 +0530

    Update script.js

--- test.html/script.js
+++ test.html/script.js
@@ -11,7 +11,7 @@ names[8] = "laura";
 names[9] = "jim";
 for(var i=0; i<names.length;i++){
     if(names[i].charAt(0)=="J"||names[i].charAt(0)=="j"){
-    console.log("Goodbye " + names[i]);
+    console.log("Good Bye + names[i]);
     }
     else{
     console.log("Hello " + names[i]);
